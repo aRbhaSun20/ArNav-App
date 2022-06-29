ffa9e47fb3e32fdf58bc48e1b9f8b89d347d194d
aRbhaSun20 <remo78295279@gmail.com>
2022-06-30 01:25:32 +0530

    changes with packages

--- src/Screens/Maps/MapContainer.js
+++ src/Screens/Maps/MapContainer.js
@@ -1,14 +1,32 @@
 import { Flex } from "native-base";
 import React, { memo } from "react";
+// import { Dimensions, StyleSheet } from "react-native";
+// import MapView from "react-native-maps";
 import BottomBar from "./BottomBar";
 
 function MapContainer({ navigation }) {
   return (
     <Flex height="100%" justifyContent="space-between">
-      <Flex>Map</Flex>
+      <Flex>
+        {" "}
+        {/* <MapView style={styles.map} /> */}
+      </Flex>
       <BottomBar navigation={navigation} />
     </Flex>
   );
 }
 
 export default memo(MapContainer);
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   map: {
+//     width: Dimensions.get("window").width,
+//     height: Dimensions.get("window").height,
+//   },
+// });
--- src/Screens/Maps/QRContainer.js
+++ src/Screens/Maps/QRContainer.js
@@ -1,14 +1,49 @@
-import { Flex } from 'native-base';
-import React from 'react'
-import BottomBar from './BottomBar';
+import { Flex } from "native-base";
+import React, { useState, useEffect } from "react";
+import BottomBar from "./BottomBar";
+import { Text, View, StyleSheet } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 const QRContainer = ({ navigation }) => {
-    return (
-      <Flex height="100%" justifyContent="space-between">
-        <Flex>QR</Flex>
-        <BottomBar navigation={navigation} />
-      </Flex>
-    );
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
 
-export default QRContainer
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <Flex height="100%" justifyContent="space-between">
+      <View style={styles.container}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      </View>
+      <BottomBar navigation={navigation} />
+    </Flex>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+});
+
+export default QRContainer;
